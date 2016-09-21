ce382bacf2202812c421ed7edef52031df30e3a1
Hitoshi Koyama <haze_sh14@yahoo.co.jp>
2016-09-21 14:01:22 +0900

    new file add

---
+++ syncrst.v
@@ -0,0 +1,26 @@
+module syncrst (
+	clk,		// Input clock 16MHz
+	rst_a,		// Asynchronus reset
+	rst_s		// Synchronus reset
+);
+
+input	clk;
+input	rst_a;
+
+output	rst_s;
+
+reg	rst_s;
+
+// internal
+reg	rst_i;
+
+//
+// Synchronus reset signal
+//
+
+always @(posedge clk) begin
+	rst_i <= ~rst_a;
+	rst_s <= rst_i;
+end
+
+endmodule
